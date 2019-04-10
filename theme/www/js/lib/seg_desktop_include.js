6690e8089f4394f5c0f7c68f9a45d2605e4dd6d8
threepwoodd <scope.dk@gmail.com>
2019-04-10 11:05:10 +0200

    Update includes

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -15,4 +15,4 @@ document.write('<script type="text/javascript" src="/js/lib/desktop/i-finale.js"
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-cookies.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2-himmelmekanik_dk/src/u-googleanalytics.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-googleanalytics.js"></script>');
