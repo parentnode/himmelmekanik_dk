e6470a8a251e39e6428c2e441edc000ad3c0965e
kaestel <martin@think.dk>
2023-03-15 20:37:31 +0100

    build

--- theme/www/js/seg_unsupported.js
+++ theme/www/js/seg_unsupported.js
@@ -1,7 +1,10 @@
+/*
+asset-builder @ 2023-03-15 20:37:16
+*/
 
 /*seg_unsupported_include.js*/
 
-/*i-unsupported.js*/
+/*m-unsupported.js*/
 function unsupported() {
 	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 15%; font-family: Arial; color: #333333">Your browser is NOT supported. It is more outdated than steam-engines, typewriters and VHS tapes, so stop acting surprised.</h1>';
 }
